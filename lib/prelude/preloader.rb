c080b6371e9bc446eb78ef510de92d325974c00d
John Crepezzi <john.crepezzi@gmail.com>
2020-05-22 18:14:48 -0400

    Allow resolvers per-object so Hash default_proc can be used with batch_size

--- lib/prelude/preloader.rb
+++ lib/prelude/preloader.rb
@@ -2,31 +2,32 @@ module Prelude
   class Preloader
     def initialize(klass, records)
       @klass = klass
-      @records_to_load = records
-      @values = Hash.new { |h, k| h[k] = {} }
+      @records = records
+
+      @resolvers = {}
     end
 
     def fetch(name, object, *args)
       method = @klass.preloaders.fetch(name)
 
-      # First check if this value already exists
-      value = values_hash_for(method, args)[object]
-      return value if value
+      # If this object has a resolver, return the value
+      if resolver = resolver_for(method, args, object)
+        return resolver[object]
+      end
 
       # Choose a batch of the correct size that contains the object we're trying to load,
       # or use all if we're not batching
-      if method.batch_size
-        slices = @records_to_load.each_slice(method.batch_size)
-        slice = slices.detect { |slice| slice.include?(object) } || slices.first
-        @records_to_load = @records_to_load.reject { |r| slice.include?(r) } # ensure we don't try to reload
+      resolver = if method.batch_size
+        remaining_records = @records.to_a - resolved_objects_for(method, args)
+        slices = remaining_records.each_slice(method.batch_size)
+        slice = slices.detect { |slice| slice.include?(object) }
         preload(method, slice, args)
       else
-        preload(method, @records_to_load, args)
-        @records_to_load = []
+        preload(method, @records, args)
       end
 
       # Return the value for this object
-      values_hash_for(method, args)[object]
+      resolver[object]
     end
 
     private
@@ -35,21 +36,27 @@ module Prelude
     def preload(method, records, args)
       results = method.call(records, *args)
 
-      if method.batch_size
-        # In the case batch size is set, merge into a results array. Also
-        # raise if default_proc is set on the Hash because we won't be able
-        # to support that
-        raise 'Cannot use default_proc with batch_size' if results.default_proc
-        values_hash_for(method, args).merge!(results)
-      else
-        # In the case batch size isn't set, use the results hash directly
-        # so that the user can use default_proc
-        @values[method][args] = results
+      # set the resolver for each of these name/record/args combos
+      records.each do |record|
+        set_resolver_for(method, args, record, results)
       end
+
+      # Return the resolver
+      results
     end
 
-    def values_hash_for(method, args)
-      @values[method][args] ||= {}
+    def resolver_for(method, args, object)
+      @resolvers.dig(method, args, object)
+    end
+
+    def resolved_objects_for(method, args)
+      @resolvers.dig(method, args)&.keys || []
+    end
+
+    def set_resolver_for(method, args, object, resolver)
+      @resolvers[method] ||= {}
+      @resolvers[method][args] ||= {}
+      @resolvers[method][args][object] = resolver
     end
   end
 end
