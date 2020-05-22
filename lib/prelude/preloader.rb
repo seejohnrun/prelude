9b677285e2ef228a1db6da6a8df94a51dc18b1a7
John Crepezzi <john.crepezzi@gmail.com>
2020-05-22 18:16:20 -0400

    Rename resolver->run

--- lib/prelude/preloader.rb
+++ lib/prelude/preloader.rb
@@ -4,20 +4,20 @@ module Prelude
       @klass = klass
       @records = records
 
-      @resolvers = {}
+      @runs = {}
     end
 
     def fetch(name, object, *args)
       method = @klass.preloaders.fetch(name)
 
-      # If this object has a resolver, return the value
-      if resolver = resolver_for(method, args, object)
-        return resolver[object]
+      # If this object has a run, return the value
+      if run = run_for(method, args, object)
+        return run[object]
       end
 
       # Choose a batch of the correct size that contains the object we're trying to load,
       # or use all if we're not batching
-      resolver = if method.batch_size
+      run = if method.batch_size
         remaining_records = @records.to_a - resolved_objects_for(method, args)
         slices = remaining_records.each_slice(method.batch_size)
         slice = slices.detect { |slice| slice.include?(object) }
@@ -27,7 +27,7 @@ module Prelude
       end
 
       # Return the value for this object
-      resolver[object]
+      run[object]
     end
 
     private
@@ -36,27 +36,27 @@ module Prelude
     def preload(method, records, args)
       results = method.call(records, *args)
 
-      # set the resolver for each of these name/record/args combos
+      # set the run for each of these name/record/args combos
       records.each do |record|
-        set_resolver_for(method, args, record, results)
+        set_run_for(method, args, record, results)
       end
 
-      # Return the resolver
+      # Return the run
       results
     end
 
-    def resolver_for(method, args, object)
-      @resolvers.dig(method, args, object)
+    def run_for(method, args, object)
+      @runs.dig(method, args, object)
     end
 
     def resolved_objects_for(method, args)
-      @resolvers.dig(method, args)&.keys || []
+      @runs.dig(method, args)&.keys || []
     end
 
-    def set_resolver_for(method, args, object, resolver)
-      @resolvers[method] ||= {}
-      @resolvers[method][args] ||= {}
-      @resolvers[method][args][object] = resolver
+    def set_run_for(method, args, object, run)
+      @runs[method] ||= {}
+      @runs[method][args] ||= {}
+      @runs[method][args][object] = run
     end
   end
 end
