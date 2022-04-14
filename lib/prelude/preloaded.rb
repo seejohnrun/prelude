bf35e0f1d50db500be8d9dac6f0ccab9c3fa9bec
George Brocklehurst <george@georgebrock.com>
2022-04-14 13:43:54 +0000

    BasicObject & better names for added safety.

--- lib/prelude/preloaded.rb
+++ lib/prelude/preloaded.rb
@@ -1,30 +1,34 @@
 module Prelude
   class ValueNotPreloaded < StandardError; end
 
-  class Preloaded
+  class Preloaded < BasicObject
     def initialize(preloadable)
       @preloadable = preloadable
     end
 
     def method_missing(name, *args)
-      if respond_to_missing?(name, false)
-        fetch(name, *args)
+      if has_prelude_method?(name)
+        fetch_preloaded_value(name, *args)
       else
         super
       end
     end
 
-    def respond_to_missing?(name, include_private)
-      @preloadable.class.prelude_methods.has_key?(name) || super
+    def respond_to_missing?(name, include_private = false)
+      has_prelude_method?(name) || super
     end
 
     private
 
-    def fetch(name, *args)
+    def has_prelude_method?(name)
+      @preloadable.class.prelude_methods.has_key?(name)
+    end
+
+    def fetch_preloaded_value(name, *args)
       key = [name, args]
       @preloadable.preloaded_values.fetch(key)
-    rescue KeyError
-      raise ValueNotPreloaded,
+    rescue ::KeyError
+      ::Kernel.raise ::Prelude::ValueNotPreloaded,
         "#{@preloadable.inspect} has no preloaded value for method " +
         "##{name} with arguments #{args.inspect}"
     end
